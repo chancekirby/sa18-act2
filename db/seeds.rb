6543b6a2c70b99bcb1826940be9ac52226fd1d45
Chance Kirby <chance14@comcast.net>
2024-04-18 22:15:35 -0500

    Seeded the database with products.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(name: 'hiking boots', price: 80.00, description: 'boots for hiking')
+Product.create!(name: 'kayak', price: 100.00, description: 'kayak for kayaking')
+Product.create!(name: 'fishing rod', price: 50.00, description: 'fishing rod for fishing')
